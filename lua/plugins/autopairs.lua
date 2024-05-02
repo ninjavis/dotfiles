151c3d05307ca88f674d2d9d24c031a128f19794
Andries van Wyk <ninjavis@gmail.com>
2024-05-02 08:19:30 +0200

    remove all files

--- lua/plugins/autopairs.lua
+++
@@ -1,6 +0,0 @@
--- https://github.com/windwp/nvim-autopairs
-return {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
-}
--- lua/plugins/mini-bufremove.lua
+++
@@ -1,2 +0,0 @@
--- https://github.com/echasnovski/mini.bufremove ( required by bufferline )
-return { 'echasnovski/mini.bufremove', version = '*' }
