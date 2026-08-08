0f6678ec56058d4dcae73d7d3fa42d53e6a6d047
Andries <ninjavis@gmail.com>
2026-08-08 18:03:26 +0200

    enable display inline diagnostics virtual text

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -5,7 +5,10 @@ vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format Local buffer" })
 vim.keymap.set("n", "df", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 
--- vim.diagnostic.config({ virtual_text = true })
+vim.diagnostic.config({
+  virtual_text = true
+})
+
 --
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 -- capabilities = vim.tbl_deep_extend("force", capabilities, require("mini.completion").get_lsp_capabilities())
