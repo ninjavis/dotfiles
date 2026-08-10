6282b71401e91e22ab0124c00f0dec81f253a2ba
Andries <ninjavis@gmail.com>
2026-08-10 10:47:26 +0200

    remap Telescope keymaps

--- lua/telescope_config.lua
+++ lua/telescope_config.lua
@@ -26,13 +26,30 @@ pcall(require('telescope').load_extension, 'file_browser') -- Loads the browser
 
 -- 3. Define telescope keymaps
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
--- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' }) -- to open Telescope help docs
+vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<C-n>', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<C-b>', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>D',"<cmd>Telescope diagnostics bufnr=0<CR>", { desc = 'Telescope help tags' })
 
-vim.keymap.set('n', '<leader>fb', ':Telescope file_browser<CR>', {
-  noremap = true,
-  desc = 'Telescope open file browser'
-})
+vim.keymap.set('n', '<C-m>', ':Telescope file_browser<CR>', { desc = 'Telescope open file browser' }) -- does not work if netrw is open, only if file is open
+
+-- Telescope file_browser default keymaps
+-- <A-c>/c 	  create	              Create file/folder at current path (trailing path separator creates folder)
+-- <S-CR>  	  create_from_prompt	  Create and open file/folder from prompt (trailing path separator creates folder)
+-- <A-r>/r	  rename	              Rename multi-selected files/folders
+-- <A-m>/m	  move	                Move multi-selected files/folders to current path
+-- <A-y>/y	  copy	                Copy (multi-)selected files/folders to current path
+-- <A-d>/d	  remove	              Delete (multi-)selected files/folders
+-- <C-o>/o	  open	                Open file/folder with default system application
+-- <C-g>/g	  goto_parent_dir	      Go to parent directory
+-- <C-e>/e	  goto_home_dir	        Go to home directory
+-- <C-w>/w	  goto_cwd	            Go to current working directory (cwd)
+-- <C-t>/t	  change_cwd	          Change nvim's cwd to selected folder/file(parent)
+-- <C-f>/f	  toggle_browser	      Toggle between file and folder browser
+-- <C-h>/h	  toggle_hidden	        Toggle hidden files/folders
+-- <C-s>/s	  toggle_all	          Toggle all entries ignoring ./ and ../
+-- <Tab>	    see telescope.nvim	  Toggle selection and move to next selection
+-- <S-Tab>	  see telescope.nvim	  Toggle selection and move to prev selection
+-- <bs>/	    backspace	            With an empty prompt, goes to parent dir. Otherwise acts normally
